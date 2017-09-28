e482ffbff3dcafdc3e8fe3fb5e730e460d221002
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-09-28 02:21:24 -0300

    damn

--- packages/joadr:khipu/lib/init.js
+++ packages/joadr:khipu/lib/init.js
@@ -1,8 +1,8 @@
 khipu = {};
 khipu.config = {
 	url: 'https://khipu.com/api/1.3/',
-	receiver_id: '26618',
-	secret: 'f078e5e44442591086ccdc9e81305eb22b99665d'
+	receiver_id: '',
+	secret: ''
 }
 
 khipu.config.return_url = "http://conejitas1.meteor.com/khipu/return";
